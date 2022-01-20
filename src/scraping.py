6897f996622e63b79562307ed9bd86ce72064c10
Rodrigo Luiz Tomio Ogava <rodrigolto@uninove.br>
2022-01-19 21:50:13 -0300

    Adicionando comentários para depois

--- src/scraping.py
+++ src/scraping.py
@@ -3,6 +3,9 @@ import requests
 
 def request_profile(user: str) -> str:
 
+    # Verificar se é possível pegar as informações através da API de eventos
+    # https://docs.github.com/en/rest/reference/activity#events
+
     url_profile = f'https://github.com/{user}/'
 
     r = requests.get(url_profile)
